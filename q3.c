984b1de8eb533ef3a7110febb513739177188c63
divyanshukumar7254-sketch <divyanshukumar7254@gmail.com>
2025-08-28 14:36:04 +0530

    Create q3.c

---
+++ q3.c
@@ -0,0 +1,17 @@
+/* Q3-> program in c  to calculate the area and perimeter of a rectangle given its length and breadth.
+*/
+
+#include <stdio.h>
+
+int main() {
+    float length, breadth, area, perimeter;
+    printf("Enter length and breadth of the rectangle: ");
+    scanf("%f %f", &length, &breadth);
+    area = length * breadth;
+    perimeter = 2 * (length + breadth);
+
+    printf("Area = %.2f\n", area);
+    printf("Perimeter = %.2f\n", perimeter);
+
+    return 0;
+}
